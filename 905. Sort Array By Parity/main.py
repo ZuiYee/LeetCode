ff5b77cf517b5b235e18f88fda074a111789e521
zuiyee <zuiyeechina@gmail.com>
2019-02-18 22:46:10 +0800

    Update main.py

--- 905. Sort Array By Parity/main.py
+++ 905. Sort Array By Parity/main.py
@@ -4,9 +4,11 @@ class Solution(object):
         :type A: List[int]
         :rtype: List[int]
         """
+        #  lambda 简单函数 x函数入口
         return sorted(A, key=lambda x: x % 2)
+
 
 if __name__ == '__main__':
     s = Solution()
-    a = [3,1,2,4]
+    a = [3, 1, 2, 4]
     print(s.sortArrayByParity(a))
